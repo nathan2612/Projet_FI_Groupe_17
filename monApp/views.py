e6809048e60d2e535d4ec5aa24ce727da0672825
Antonin <antonin.lubin@etu.univ-orleans.fr>
2025-10-23 10:19:46 +0200

    Ajout de la page contact

--- monApp/views.py
+++ monApp/views.py
@@ -7,5 +7,9 @@ from .app import db
 def index():
     return render_template("index.html")
                        
+@app.route('/contact/')
+def contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run()
