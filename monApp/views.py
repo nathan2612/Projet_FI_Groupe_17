2b019ec8fe3ddeda1741332bc10cca6ccee2c305
nathan2612 <nathan.joubert@etu.univ-orleans.fr>
2025-11-06 11:55:14 +0100

    commencement de la page commande

--- monApp/views.py
+++ monApp/views.py
@@ -95,61 +95,9 @@ def produits():
         }
     )
 
-#@app.route('/menus/', methods=['POST', 'GET'])
-#def menus():
-#    cat_id = request.args.get('cat_id', type=int)
-#    page = request.args.get('page', 1, type=int)
-#    per_page = 9
-#
-#    # Build base query with optional category filter
-#    query = db.session.query(PLAT)
-#    if cat_id is not None:
-#        query = query.filter_by(id_categorie=cat_id)
-#
-#    vegetarien = request.values.get('vegetarien', '0') == '1'
-#    vegan = request.values.get('vegan', '0') == '1'
-#    sans_gluten = request.values.get('sans_gluten', '0') == '1'
-#    sans_lactose = request.values.get('sans_lactose', '0') == '1'
-#    sans_fruits_a_coque = request.values.get('sans_fruits_a_coque', '0') == '1'
-#    sans_crustaces = request.values.get('sans_crustaces', '0') == '1'
-#
-#    if vegetarien:
-#        query = query.filter(PLAT.vegetarien.is_(True))
-#    if vegan:
-#        query = query.filter(PLAT.vegan.is_(True))
-#    if sans_gluten:
-#        query = query.filter(PLAT.gluten.is_(False))
-#    if sans_lactose:
-#        query = query.filter(PLAT.lactose.is_(False))
-#    if sans_fruits_a_coque:
-#        query = query.filter(PLAT.fruits_a_coque.is_(False))
-#    if sans_crustaces:
-#        query = query.filter(PLAT.crustaces.is_(False))
-#
-#    total = query.count()
-#    total_pages = max(1, ceil(total / per_page))
-#    page = max(1, min(page, total_pages))
-#    produits = query.offset((page - 1) * per_page).limit(per_page).all()
-#    categories = db.session.query(CATEGORIE).all()
-#
-#    return render_template(
-#        "produits.html",
-#        produits=produits,
-#        cat_id=cat_id,
-#        categories=categories,
-#        page=page,
-#        total_pages=total_pages,
-#        total_items=total,
-#        per_page=per_page,
-#        filters={
-#            'vegetarien': vegetarien,
-#            'vegan': vegan,
-#            'sans_gluten': sans_gluten,
-#            'sans_lactose': sans_lactose,
-#            'sans_fruits_a_coque': sans_fruits_a_coque,
-#            'sans_crustaces': sans_crustaces,
-#        }
-#    )
+@app.route('/menus/', methods=['POST', 'GET'])
+def menus():
+    return render_template("menus.html")
 
 @app.route('/produit/<int:id_plat>')
 def detail_plat(id_plat):
