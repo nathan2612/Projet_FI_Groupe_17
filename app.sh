8f0ba8c27df73bb5588488795da9dbe36ecbabe5
louismaillet <louis.maillet@etu.univ-orleans.fr>
2026-02-11 16:16:19 +0100

    requirement sans s

--- app.sh
+++ app.sh
@@ -1,7 +1,7 @@
 cd Projet_FI_Groupe_17/
 python3 -m venv venv
 source venv/bin/activate
-pip install -r requirements.txt
+pip install -r requirement.txt
 export FLASK_APP=monApp
 export FLASK_ENV=development
 flask loaddb monApp/data/data.yml
